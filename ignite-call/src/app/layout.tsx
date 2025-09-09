a1c9a4d73006c1b2ab6d89184d1555cec87a2990
Guilherme Cataneo <guilhermecataneo02@gmail.com>
2025-09-08 23:33:44 -0300

    clear defaults files Next

--- ignite-call/src/app/layout.tsx
+++ ignite-call/src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const RobotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const RobotoFont = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
 });
 
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${RobotoMono.variable} ${RobotoFont.variable} antialiased`}>
         {children}
       </body>
     </html>
