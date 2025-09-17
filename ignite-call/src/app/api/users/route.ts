cfca948db4603d29af568e096bbd308a84d917d3
Guilherme Cataneo <guilhermecataneo02@gmail.com>
2025-09-17 08:21:06 -0300

    :sparkles: NextAuth file, and refactor user route to use NextRequest

--- ignite-call/src/app/api/users/route.ts
+++ ignite-call/src/app/api/users/route.ts
@@ -1,38 +1,39 @@
 import { prisma } from "@/src/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
-import { NextApiResponse, NextApiRequest } from "next";
 import { setCookie } from "nookies";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   try {
-  const { username, name } = req.body;
+    const { username, name } = await req.json();
 
+    const userExists = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
 
+    if (userExists) {
+      return NextResponse.json(
+        { error: "Username already exists" },
+        { status: 400 }
+      );
+    }
 
-  const userExists = await prisma.user.findUnique({
-    where: {
-      username,
-    },
-  });
+    const user = await prisma.user.create({
+      data: { username, name },
+    });
 
-  if (userExists) {
-    return NextResponse.json({ error: "Username already exists" }, { status: 400 });
-  }
-
-  const user = await prisma.user.create({
-    data: { username, name },
-  });
-
-  setCookie({ res }, "user", user.id, 
-    {
+    setCookie({ res }, "user", user.id, {
       maxAge: 60 * 60 * 24 * 7, // 7 days
       path: "/",
-    }
-  );
+    });
 
-  return NextResponse.json(user, { status: 201 });
+    return NextResponse.json(user, { status: 201 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
--- ignite-call/src/app/registration/2/FormGoogleAgend.tsx
+++ ignite-call/src/app/registration/2/FormGoogleAgend.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { ArrowRight } from "@phosphor-icons/react";
+import { signIn } from "next-auth/react";
+
 export function FormGoogleAgend() {
   return (
     <div className="w-full flex flex-col flex-wrap gap-6">
       <div className="w-full flex flex-row flex-wrap items-center justify-between border-1 border-[#323238] p-4 rounded-lg">
         <h1>Google Agenda</h1>
 
-        <button className="w-28 h-10 bg-transparent rounded-lg p-4 text-white flex items-center justify-center gap-2 border-solid border-2 border-[#00B37E] px-6 py-3">
+        <button
+          className="w-28 h-10 bg-transparent rounded-lg p-4 text-white flex items-center justify-center gap-2 border-solid border-2 border-[#00B37E] px-6 py-3 "
+          onClick={() => signIn("google")}
+        >
           <span className="flex items-center justify-center gap-2 text-[#00B37E] font-semibold">
             Conectar <ArrowRight size={16} className="fill-[#00B37E]" />
           </span>
